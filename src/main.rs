39a81ddfe0468938fa4b4fa1a10ab8e008136998
Jesse Alford <jalford@pivotal.io>
2023-07-16 11:34:52 -0700

    Take visitor name as input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io::stdin;
+
 fn main() {
     println!("This is an automated treehouse. Govern Yourself Accordingly. IDENTIFY YOURSELF.");
+
+    let mut visitor_name = String::new();
+    stdin()
+        .read_line(&mut visitor_name)
+        .expect("Failed to read line:");
 }
