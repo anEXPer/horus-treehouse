d3b590a3291378d8f7033a66a89a673a41463fd7
Jesse Alord <jesse.alford@pm.me>
2023-12-19 17:29:32 -0800

    Add a custom per-visitor greeting
    - we're storing these as structs
    - not happy with how it's passed around by newing something up,
      and clippy has lots to say.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,48 @@
 use std::io::stdin;
 
 fn main() {
+    let mut guest_allowed = false;
+    let mut confirmed_visitor = Visitor::new("", "");
+
+    let visitors_list = [
+        Visitor::new("steve", "Hello Steve!"),
+        Visitor::new("bert", "Hello Bert you maniac!"),
+        Visitor::new("riz", "Hello Riz, long time no see!"),
+    ];
+
+    struct Visitor {
+        name: String,
+        greeting: String,
+    }
+
+    impl Visitor {
+        fn new(name: &str, greeting: &str) -> Self {
+            Self {
+                name: name.to_lowercase(),
+                greeting: greeting.to_string(),
+            }
+        }
+        fn greet(&self) {
+            println!("{}", self.greeting);
+        }
+    }
+
     println!("This is an automated treehouse. Govern Yourself Accordingly. IDENTIFY YOURSELF.");
 
-    let mut visitor_name = String::new();
-    stdin()
-        .read_line(&mut visitor_name)
-        .expect("Failed to read line:");
-    visitor_name = visitor_name.trim().to_lowercase();
+    let mut guest = String::new();
+    stdin().read_line(&mut guest).expect("Failed to read line:");
+    guest = guest.trim().to_lowercase();
 
-    if visitor_name == "bert" {
-        println!("Hello, {visitor_name}. Welcome.");
+    for visitor in &visitors_list {
+        if visitor.name == guest {
+            guest_allowed = true;
+            confirmed_visitor = Visitor::new(&visitor.name, &visitor.greeting);
+        }
+    }
+
+    if guest_allowed {
+        confirmed_visitor.greet()
     } else {
-        println!("Sorry, {visitor_name}, you are not on the list.");
+        println!("Sorry, {0}, you are not on the list.", guest);
     }
 }
