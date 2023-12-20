be30b37d7d3be6c65b0cd2701bde8bd16f4f38cf
Jesse Alord <jesse.alford@pm.me>
2023-12-20 09:36:04 -0800

    Clippy wants all the declarations of items first

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,8 @@
 use std::io::stdin;
 
 fn main() {
-    let mut guest_allowed = false;
-    let mut confirmed_visitor = Visitor::new("", "");
 
-    let visitors_list = [
-        Visitor::new("steve", "Hello Steve!"),
-        Visitor::new("bert", "Hello Bert you maniac!"),
-        Visitor::new("riz", "Hello Riz, long time no see!"),
-    ];
+    // statements: declarations
 
     struct Visitor {
         name: String,
@@ -28,6 +22,19 @@ fn main() {
         }
     }
 
+    // statements: variables
+
+    let mut guest_allowed = false;
+    let mut confirmed_visitor = Visitor::new("", "");
+
+    let visitors_list = [
+        Visitor::new("steve", "Hello Steve!"),
+        Visitor::new("bert", "Hello Bert you maniac!"),
+        Visitor::new("riz", "Hello Riz, long time no see!"),
+    ];
+
+    // expressions: behavior
+
     println!("This is an automated treehouse. Govern Yourself Accordingly. IDENTIFY YOURSELF.");
 
     let mut guest = String::new();
@@ -42,8 +49,8 @@ fn main() {
     }
 
     if guest_allowed {
-        confirmed_visitor.greet()
+        confirmed_visitor.greet();
     } else {
-        println!("Sorry, {0}, you are not on the list.", guest);
+        println!("Sorry, {guest}, you are not on the list.");
     }
 }
