decfe302edc3a3d112eb7a9b8eeeff23e72b6bec
Jesse Alord <jesse.alford@pm.me>
2024-01-21 14:56:39 -0800

    Add a test for an alterate "true" binary
    - Still basically just learning testing stuff here
    - I don't think we're testing exit values still, just builds!
      But I am not sure.

---
+++ src/bin/true.rs
@@ -0,0 +1,5 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+fn main() {
+    std::process::exit(0)
+}
--- tests/cli.rs
+++ tests/cli.rs
@@ -3,6 +3,11 @@ use assert_cmd::Command;
 #[test]
 fn works() {
     let mut cmd = Command::cargo_bin("treehouse").unwrap();
-    let res = cmd.output();
+    cmd.assert().success();
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
     cmd.assert().success();
 }
